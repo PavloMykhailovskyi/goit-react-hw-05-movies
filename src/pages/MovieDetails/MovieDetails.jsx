e070a5d0ca530ae7ca7103e1329b232183fc69b8
PavloMykhailovskyi <100272784+PavloMykhailovskyi@users.noreply.github.com>
2022-11-19 23:30:59 +0200

    add

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -39,26 +39,31 @@ const MovieDetails = () => {
 
     return (
       <main className={css.main}>
-        <p>
-          <NavLink
-            to={refLocation.current.state?.from ?? '/'}
-            className={css.link_back}
-          >
-            Get back
-          </NavLink>
-        </p>
-        <img
-          src={`https://image.tmdb.org/t/p/w500${backdrop_path}`}
-          alt={title}
-        />
-        <h2>
-          {title} {new Date(release_date).getFullYear()}
-        </h2>
-        <p>User score: {Math.floor(vote_average * 10)}%</p>
-        <h3>Overview:</h3>
-        <p>{overview}</p>
-        <h3>Genres:</h3>
-        <p>{genres ? genres.map(genre => genre.name).join(' ') : '-'}</p>
+        <div className={css.wrap}>
+          <p>
+            <NavLink
+              to={refLocation.current.state?.from ?? '/'}
+              className={css.link_back}
+            >
+              Get back
+            </NavLink>
+          </p>
+          <img
+            className={css.image}
+            src={`https://image.tmdb.org/t/p/w500${backdrop_path}`}
+            alt={title}
+          />
+          <div>
+            <h2>
+              {title} {new Date(release_date).getFullYear()}
+            </h2>
+            <p>User score: {Math.floor(vote_average * 10)}%</p>
+            <h3>Overview:</h3>
+            <p>{overview}</p>
+            <h3>Genres:</h3>
+            <p>{genres ? genres.map(genre => genre.name).join(' ') : '-'}</p>
+          </div>
+        </div>
         <h2>Additional information</h2>
         <ul className={css.list}>
           <li className={css.list_item}>
